9e6b493971215fc82752e928d8dbf2b4187f3b33
Varun Prasannan <68280782+varunprasannan@users.noreply.github.com>
2024-02-02 18:50:40 +0530

    Added new Paper link and changed Work Experience section

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ with open("VarunPrasannan.pdf", "rb") as pdf_file:
 
 st.download_button(label="Download Resume",
                     data=PDFbyte,
-                    file_name="VarunPrasannanResume.pdf",
+                    file_name="VarunPrasannan.pdf",
                     mime='application/octet-stream')
 
 st.markdown("***")
@@ -57,27 +57,46 @@ with st.sidebar:
     st.link_button('LinkedIn','https://www.linkedin.com/in/varunprasannan/', use_container_width=True)
 
 
-with st.container():
-    st.title("📚 Education")
-    st.text(" ")
-    st.markdown(":blue[Bachelor of Engineering] in :blue[Information Technology] from **:blue[Pimpri Chinchwad College of Engineering]**")
-    st.markdown(":blue[HSC State Board] from **:blue[C.M.S English Medium High School]**")
-    st.markdown(":blue[SSC State Board] from **:blue[C.M.S English Medium High School]**")
-st.markdown("***")
-
-
 # WORK EXPERIENCE
 with st.container():
     st.title("👔 Work Experience")
     st.text(" ")
     with st.expander('AFour Technologies Pvt. Ltd.'):
-        st.write("• Created various Flask apps using Python")
-        st.write("• Understood REST api concepts and development")       
-        st.write("• Developed applications using Streamlit Framework for Python")
+        st.write("""
+:blue[Project: Snowflake Accelerator (Python, Streamlit, Data Visualization)]
+- Developed various functionalities with exclusive access to the company's Snowflake data files.
+- Created visually engaging charts and visualizations using Matplotlib, Altair to display usage statistics.
+- Transformed raw usage data into meaningful graphs, charts, and diagrams that provide users with insights
+into their account's activity.\n
+                 
+:blue[Project: AFrosty - AI Chatbot for your databases (Python, Streamlit, Snowflake, OpenAI API, GenAI)]
+- Led the Proof of Concept (PoC) with access to the company's private data and developed a chatbot which will answer questions based on your database by extracting metadata from the database such as table names,
+number of columns, etc.
+- This project will help developers to generate sql queries just by using natural language and will also fire the
+generated query and generate results in the user interface.\n
+
+:blue[Project: Snowflake - Google Sheets Connector (Python, Streamlit, Snowflake API)]
+- Developed an application which will push data from your personal google sheet file into your Snowflake
+database at regular intervals defined by the user.
+- This project will enable users to use the data for various visualizations using snowflake and also secures the
+data in your Google Sheet files by backing up into the Snowflake Data Warehouse.
+""")
 
     with st.expander('Intoit Solutions Pvt. Ltd.'):
-        st.write("• Part of the Web development and IT team and designed the official website")
-        st.write("• Designed the official website for the company")       
+        st.write("""
+- Designed and developed the company's official website, showcasing a comprehensive grasp of modern web technologies utilizing HTML, CSS, and Javascript to craft a robust foundation, ensuring an intuitive and
+responsive user interface.
+- Implemented dynamic features and interactive elements with React, elevating the website's functionality and
+user experience.
+- Employed TailwindCSS and Bootstrap to streamline the styling process, ensuring a polished and visually
+appealing design.
+- Collaborated closely with cross-functional teams to gather requirements and incorporate feedback, ensuring
+alignment with company branding and objectives.
+- Employed best practices in web design, such as responsive design principles, to guarantee optimal user
+experiences across various devices.
+- Engaged in continuous learning and exploration of emerging technologies, staying at the forefront of industry
+trends to enhance the website's design and performance.
+""")
  
     with st.expander('The Sparks Foundation'):
         st.write("• Developed a basic banking system website with various functions using Django, Python, HTML and CSS.")
@@ -85,6 +104,15 @@ with st.container():
         st.write("• Received a Letter of Recommendation for the same")
 st.markdown("***")
 
+
+#EDUCATION
+with st.container():
+    st.title("📚 Education")
+    st.text(" ")
+    st.markdown(":blue[Bachelor of Engineering] in :blue[Information Technology] from **:blue[Pimpri Chinchwad College of Engineering]**")
+    st.markdown(":blue[HSC State Board] from **:blue[C.M.S English Medium High School]**")
+    st.markdown(":blue[SSC State Board] from **:blue[C.M.S English Medium High School]**")
+st.markdown("***")
 
 # KEY PROJECTS
 with st.container():
@@ -133,7 +161,7 @@ with st.container():
     st.title("🔬 Research Papers")
     st.text(" ")
     with st.expander('Smart Computer Commands using Gesture Recognition - PCCDA 2023'):
-        st.write("Paper available [here](https://link.springer.com/chapter/10.1007/978-981-19-6088-8_44)")
+        st.write("Paper available [here](https://link.springer.com/chapter/10.1007/978-981-99-4626-6_21)")
     
     with st.expander('Gesture Recognition and conversion to speech for Specially Abled - ICMISC 2022'):
         st.write("Paper available [here](https://link.springer.com/chapter/10.1007/978-981-19-6088-8_44)")
